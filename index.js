aa90b7afa5d1c8099d05542bb745e5903d0eb91b
peripheralist <peripheralist@gmail.com>
2021-10-30 13:58:54 -0400

    -

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(path.resolve("", "index.html")));
+  res.sendFile(path.join(path.resolve("public", "index.html")));
 });
 
 app.get("/bundle.js", function (req, res) {
-  res.sendFile(path.join(path.resolve("", "bundle.js")));
+  res.sendFile(path.join(path.resolve("public", "bundle.js")));
 });
 
 app.listen(port, () => {
